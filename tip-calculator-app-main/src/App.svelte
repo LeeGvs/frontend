f6b79427d98235e4abee2d0bd2c350f28d15ffac
LeeG <Lee.Greaves@gmail.com>
2022-05-04 15:24:43 +0100

    initial project structure for tip calc

---
+++ tip-calculator-app-main/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<main>Bill Select Tip % 5% 10% 15% 25% 50% Custom Number of People Tip Amount / person Total / person Reset</main>
+<footer>
+  <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded by
+    <a href="#">Lee Greaves</a>.
+  </div>
+</footer>
+
+<style>
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
